5a5e8534dd3af6e1d5ca3d8cc68736a9691b4c47
rocky-36 <97944275+rocky-36@users.noreply.github.com>
2022-02-25 17:41:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+
+
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+  document.querySelectorAll("button")[i].addEventListener("click",function (){
+
+      var inputKey = this.innerHTML;
+      makeSound(inputKey);
+      shine(inputKey);
+  });
+}
+
+document.addEventListener("keypress",function(event) {
+  makeSound(event.key);
+  shine(event.key);
+})
+
+
+
+function makeSound(inputKey)
+{
+  switch (inputKey) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+    break;
+    case "a":
+      var sound = new Audio("sounds/tom-2.mp3");
+      sound.play();
+    break;
+    case "s":
+      var sound = new Audio("sounds/tom-3.mp3");
+      sound.play();
+    break;
+    case "d":
+      var sound = new Audio("sounds/tom-4.mp3");
+      sound.play();
+    break;
+    case "j":
+      var sound = new Audio("sounds/snare.mp3");
+      sound.play();
+    break;
+    case "k":
+      var sound = new Audio("sounds/crash.mp3");
+      sound.play();
+    break;
+    case "l":
+      var sound = new Audio("sounds/kick-bass.mp3");
+      sound.play();
+    break;
+    default:
+    break;
+  }
+}
+
+
+function shine(inputKey){
+  var some = document.querySelector("."+inputKey);
+  some.classList.add("pressed");
+
+  setTimeout(function() {some.classList.remove("pressed")},100);
+}
